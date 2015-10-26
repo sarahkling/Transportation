d8464c5859181fb9912204cdcd6ada162a48077f
sarahkling <Sarah@Sarahs-MacBook-2.local>
2015-10-26 13:14:01 -0400

    part 2 added

---
+++ transportation part 2/Assets/GameController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	int xmax = 16;
+	int ymax = 9;
+	int[,] sky = new int[16, 9];
+	
+	public GameObject cubePrefab;
+
+	// Use this for initialization
+	void Start () {
+
+	for(int xaxis = 0; xaxis < xmax; xaxis++) {
+		for(int yaxis = 0; yaxis < ymax; yaxis++) {
+			sky[xaxis,yaxis] = (GameObject) Instantiate(cubePrefab, new Vector3(xaxis*2-15, yaxis, 12), Quaternion.identity);
+		}
+	}
+
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
